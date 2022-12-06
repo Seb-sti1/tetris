eb29dacdf6f89c146b42ce724e45ec6e16f20a6f
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2022-12-06 11:22:55 +0100

    base structure

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tetris
+
+
+
+
+# Development
+
+## Installation
+
+Install of GTK :
+`sudo apt install libgtk-3-dev`
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by seb on 06/12/22.
+//
+
+
+public int main() {
+    std::out << "Hello, World!" << std::endl;
+
+    return 0;
+}
